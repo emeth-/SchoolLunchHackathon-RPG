09eb25464d0d5153a464814fcdc3bf1f6e0c7ccf
Emeth <seanybob@gmail.com>
2016-02-20 12:21:58 -0600

    updated url structure

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='/static/index.html')),
     url(r'^save_form$', "api.views.save_form"),
     url(r'^get_completed_forms$', "api.views.get_completed_forms"),
 )
