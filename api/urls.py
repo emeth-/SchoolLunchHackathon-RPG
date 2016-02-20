4e37b9bacb2a40f28c3cfadd0fc61238d66893b1
Emeth <seanybob@gmail.com>
2016-02-20 11:30:06 -0600

    added api calls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^save_fish$', "api.views.save_fish"),
-    url(r'^delete_fish$', "api.views.delete_fish"),
-    url(r'^get_fish$', "api.views.get_fish"),
+    url(r'^save_form$', "api.views.save_form"),
+    url(r'^get_completed_forms$', "api.views.get_completed_forms"),
 )
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import datetime
 import json
-from api.models import Fish
+from api.models import CompletedForm
 
 def json_custom_parser(obj):
     if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
@@ -10,70 +10,35 @@ def json_custom_parser(obj):
     else:
         raise TypeError(obj)
 
-def download_pdf(request):
-    from pyPdf import PdfFileWriter, PdfFileReader
-    import StringIO
-    from reportlab.pdfgen import canvas
-    from reportlab.lib.pagesizes import letter
-
-    packet = StringIO.StringIO()
-    # create a new PDF with Reportlab
-    can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(10, 100, "Hello world")
-    can.drawString(100,750,"Welcome to Reportlab!")
-    can.save()
-
-    #move to the beginning of the StringIO buffer
-    packet.seek(0)
-    new_pdf = PdfFileReader(packet)
-    # read your existing PDF
-    existing_pdf = PdfFileReader(file("original.pdf", "rb"))
-    output = PdfFileWriter()
-    # add the "watermark" (which is the new pdf) on the existing page
-    page = existing_pdf.getPage(0)
-    page.mergePage(new_pdf.getPage(0))
-    output.addPage(page)
-    # finally, write "output" to a real file
-    outputStream = file("destination.pdf", "wb")
-    output.write(outputStream)
-    outputStream.close()
-
-
-def delete_fish(request):
+def save_form(request):
     """
-        Expects as input:
-            - An id located in:
-                - request.body if Angular.js
-                - request.POST['id'] if backbone / jquery
-    """
-    fish_id = request.body #change this depending on how your frontend sends data, as POST or in the body
-
-    Fish.objects.filter(id=fish_id).delete()
-    return HttpResponse(json.dumps({
-        "status": "success"
-    }, default=json_custom_parser), content_type='application/json', status=200)
-
-def save_fish(request):
-    """
-        Expects as input:
-            - A json string location in:
-                - request.body if Angular.js
-                - request.POST['fish_info'] if backbone / jquery
+        Expect Input:
+            request.POST['data'] = A json string of the form data:
     """
 
-    fish_info = request.body #change this depending on how your frontend sends data, as POST or in the body
+    nf = CompletedForm(**{
+        "data": request.POST['data']
+    })
+    nf.save()
 
-    fsh = Fish(**json.loads(fish_info))
-    fsh.save()
     return HttpResponse(json.dumps({
         "status": "success",
-        "id": fsh.id,
-        "data": list(Fish.objects.filter(id=fsh.id).values())[0] #lololol
+        "id": nf.id,
     }, default=json_custom_parser), content_type='application/json', status=200)
 
-def get_fish(request):
-    fishies = Fish.objects.all()
+def get_completed_forms(request):
+    completed_forms = []
+    for c in CompletedForm.objects.all():
+        try:
+            completed_forms.append({
+                "data": json.loads(c.data),
+                "created": c.created
+            })
+        except ValueError:
+            #Not valid json, just skip it.
+            pass
+
     return HttpResponse(json.dumps({
         "status": "success",
-        "data": list(fishies.values())
+        "data": completed_forms
     }, default=json_custom_parser), content_type='application/json', status=200)
